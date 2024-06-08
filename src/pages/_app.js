41c6deb69d7a6d364ed97e54617ab1afe05d4d09
rittikbasu <irittik@gmail.com>
2024-06-08 23:25:29 +0530

    feat: add work sans font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
+import { Work_Sans } from "next/font/google";
 import "@/styles/globals.css";
 
+const roboto = Work_Sans({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
