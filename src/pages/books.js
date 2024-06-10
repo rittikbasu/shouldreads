5d9f3673b81efe65a4593ed91e1f6072d984f24d
rittikbasu <irittik@gmail.com>
2024-06-10 08:10:23 +0530

    feat: add aceternity search bar + add ratings + authors to filters

--- src/pages/books.js
+++ src/pages/books.js
@@ -5,16 +5,27 @@ import clsx from "clsx";
 
 import { FaHeart, FaRegComment, FaAmazon, FaChevronDown } from "react-icons/fa";
 import { IoIosStats, IoMdStar } from "react-icons/io";
-import { IoSearch } from "react-icons/io5";
-import { FaArrowLeftLong } from "react-icons/fa6";
+import { MdClear } from "react-icons/md";
 
-import { init } from "../../utils/db";
+import { init } from "@/utils/db";
+import { Searchbar } from "@/components/Searchbar";
 
 export default function Home({ data }) {
-  const [query, setQuery] = useState("");
   const [results, setResults] = useState(data);
   const [aiToggle, setAiToggle] = useState(false);
   const [filter, setFilter] = useState("");
+  const searchPlaceholders = [
+    "The Beginning of Infinity",
+    "George Orwell",
+    "Fyodor Dostoevsky",
+    "Fahrenheit 451",
+  ];
+  const aiPlaceholders = [
+    "Books like 1984",
+    "Dystopian novels",
+    "Books about philosophy",
+    "Classic literature",
+  ];
 
   const handleSearch = (query) => {
     const filteredResults = data.filter(
@@ -38,7 +49,6 @@ export default function Home({ data }) {
   };
 
   const handleAISearch = async (query) => {
-    console.log(query);
     const response = await fetch("/api/search", {
       method: "POST",
       body: JSON.stringify({ query }),
@@ -65,19 +75,17 @@ export default function Home({ data }) {
   };
 
   const handleInputChange = (e) => {
-    const newQuery = e.target.value;
-    setQuery(newQuery);
     if (!aiToggle) {
-      handleSearch(newQuery);
+      handleSearch(e.target.value);
     }
   };
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       if (aiToggle) {
-        handleAISearch(query);
+        handleAISearch(e.target.value);
       } else {
-        handleSearch(query);
+        handleSearch(e.target.value);
       }
     }
   };
@@ -86,7 +94,7 @@ export default function Home({ data }) {
     const selectedFilter = e.target.value;
     setFilter(selectedFilter);
 
-    const sortedResults = [...results].sort((a, b) => {
+    const sortedResults = [...data].sort((a, b) => {
       switch (selectedFilter) {
         case "mentions":
           return b.mentions - a.mentions;
@@ -96,12 +104,25 @@ export default function Home({ data }) {
           return b.comments - a.comments;
         case "impressions":
           return b.impressions - a.impressions;
+        case "ratings":
+          return Number(b.ratings) - Number(a.ratings);
+        case "topAuthors":
+          // Sort by mentions first, then by the number of books
+          return (
+            b.author_mentions - a.author_mentions ||
+            b.author_books - a.author_books
+          );
         default:
           return 0;
       }
     });
 
     setResults(sortedResults);
+  };
+
+  const clearFilter = () => {
+    setFilter("");
+    setResults(data);
   };
 
   const formatImpressions = (impressions) => {
@@ -111,104 +132,81 @@ export default function Home({ data }) {
   };
 
   return (
-    <div className="flex flex-col items-center min-h-screen px-8 pb-8 pt-4 md:pt-8">
+    <>
       <Head>
         <meta name="description" content="find must read books" />
         <title>shouldreads</title>
       </Head>
-      <div className="relative w-full flex justify-center items-center max-w-3xl">
-        <Link
-          href="/"
-          className="absolute left-0 top-1/2 transform -translate-y-1/2"
-        >
-          <FaArrowLeftLong className="text-blue-400 h-6 w-6 md:h-10 md:w-10" />
-        </Link>
-        <h1 className="text-zinc-200 text-3xl md:text-5xl font-medium font-mono">
-          shouldreads
-        </h1>
-      </div>
-      <div className="flex flex-col justify-center items-center mt-8 md:mt-16 md:mb-8 w-full transition-transform duration-500 ">
-        <label className="block text-gray-500 mb-2">
+      <div className="sticky top-0 w-full max-w-md z-20 mt-8 md:mt-16">
+        <label className="block text-center text-gray-500 mb-2">
           {aiToggle
             ? "what kind of book are you looking for?"
             : "search for a book or an author"}
         </label>
-        <div className="flex flex-col w-full md:w-auto sticky top-0">
-          <div className="flex w-full">
-            <input
-              type="text"
-              value={query}
-              onChange={handleInputChange}
-              onKeyDown={handleKeyDown}
-              className="pl-4 pr-1 py-2 md:w-72 w-full bg-zinc-900 border-y border-l border-zinc-800 rounded-l-xl outline-none placeholder:text-gray-500"
-              placeholder={aiToggle ? "ai search..." : "search..."}
-            />
-            <button
-              onClick={() =>
-                aiToggle ? handleAISearch(query) : handleSearch(query)
-              }
-              className="px-4 py-2 md:w-24 bg-blue-500 text-zinc-2003 rounded-r-xl hover:bg-blue-600 flex justify-center items-center group"
+        <Searchbar
+          placeholders={aiToggle ? aiPlaceholders : searchPlaceholders}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          onSubmit={aiToggle ? handleAISearch : handleSearch}
+          aiToggle={aiToggle}
+          setAiToggle={setAiToggle}
+        />
+      </div>
+      <div className="flex justify-between items-center mt-4 w-full max-w-md transition-transform duration-500">
+        <div
+          className={clsx(
+            "flex items-center sm:min-w-64 px-1.5 py-1 bg-zinc-600/50 text-sm text-gray-400 border rounded-lg border-zinc-700",
+            aiToggle && "min-w-[13rem]"
+          )}
+        >
+          <div className="relative flex-1">
+            <select
+              value={filter}
+              onChange={handleFilterChange}
+              className={clsx(
+                " outline-none appearance-none w-full bg-transparent border-zinc-600",
+                aiToggle ? "border-r " : "rounded-lg sm:border-r"
+              )}
             >
-              <span className="hidden md:block">Search</span>
-              <IoSearch className="block md:hidden h-5 w-5 group-active:scale-75 transition duration-300" />
-            </button>
+              <option value="">filters</option>
+              <option value="topAuthors">top authors</option>
+              <option value="ratings">top ratings</option>
+              <option value="mentions">top mentions</option>
+              <option value="likes">top likes</option>
+              <option value="comments">top comments</option>
+              <option value="impressions">top impressions</option>
+            </select>
+            <FaChevronDown className="absolute right-2 h-2.5 w-2.5 top-1/2 transform -translate-y-1/2 text-gray-300 pointer-events-none" />
           </div>
-          <div className="flex justify-between items-center w-full md:w-auto mt-4">
-            <div className="relative min-w-32 md:w-auto">
-              <select
-                value={filter}
-                onChange={handleFilterChange}
-                className="px-2 py-1 bg-zinc-900 border border-zinc-800 rounded-lg text-sm text-gray-500 outline-none appearance-none w-full"
-              >
-                <option value="">filters</option>
-                <option value="mentions">top mentions</option>
-                <option value="likes">top likes</option>
-                <option value="comments">top comments</option>
-                <option value="impressions">top impressions</option>
-                <option value="ratings">top ratings</option>
-              </select>
-              <FaChevronDown className="absolute right-1 h-2.5 w-2.5 top-1/2 transform -translate-y-1/2 text-gray-500 pointer-events-none" />
-            </div>
-            {aiToggle && (
-              <div className="relative min-w-32 md:w-auto">
-                <select className="px-2 py-1 bg-zinc-900 border border-zinc-800 rounded-lg text-sm text-gray-500 outline-none appearance-none w-full">
-                  <option value="">results</option>
-                  <option value="mentions">10</option>
-                  <option value="likes">20</option>
-                  <option value="comments">50</option>
-                </select>
-                <FaChevronDown className="absolute right-1 h-2.5 w-2.5 top-1/2 transform -translate-y-1/2 text-gray-500 pointer-events-none" />
-              </div>
+          <div
+            className={clsx(
+              "relative",
+              aiToggle ? "flex-1" : "hidden sm:block sm:flex-1"
             )}
-            <div className="flex items-center">
-              <span
-                className={clsx(
-                  "text-gray-500 text-sm mr-2 transition-opacity duration-1000",
-                  aiToggle ? "hidden" : "block"
-                )}
-              >
-                AI Search
-              </span>
-              <label className="relative inline-flex items-center cursor-pointer">
-                <input
-                  type="checkbox"
-                  checked={aiToggle}
-                  onChange={() => setAiToggle(!aiToggle)}
-                  className="sr-only peer"
-                />
-                <div className="w-11 h-6 bg-zinc-600 rounded-full peer peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-transform peer-checked:bg-blue-600"></div>
-              </label>
-            </div>
+          >
+            <select className="bg-transparent outline-none appearance-none w-full ml-1">
+              <option value="">results</option>
+              <option value="mentions">10</option>
+              <option value="likes">20</option>
+              <option value="comments">50</option>
+            </select>
+            <FaChevronDown className="absolute right-0.5 h-2.5 w-2.5 top-1/2 transform -translate-y-1/2 text-gray-300 pointer-events-none" />
           </div>
         </div>
+        <button
+          onClick={clearFilter}
+          className="text-blue-500/80 text-sm rounded-lg flex items-center ml-auto"
+        >
+          <MdClear className="mr-1" /> clear filters
+        </button>
       </div>
-      <div className="flex place-items-center absolute -top-44 left-1/2 transform -translate-x-1/2 -z-10 before:h-[300px] before:w-[300px] before:translate-x-0 before:rounded-full before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[240px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:blur-2xl after:content-[''] before:bg-gradient-to-br before:from-transparent before:to-blue-700/10 after:from-sky-900 after:via-[#0141ff]/40 before:lg:h-[360px]"></div>
+
       {results && (
-        <div className="mt-8 w-full max-w-3xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {results.map((result, index) => (
+        <div className="mt-8 sm:mt-12 w-full max-w-3xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {results.map((book, index) => (
             <div
-              key={result.id}
-              className="relative p-4 bg-zinc-900 rounded-2xl shadow-md border border-zinc-800 hover:border-blue-500 group flex flex-col justify-between hover:scale-110 transition duration-500"
+              key={book.id}
+              className="relative p-4 bg-zinc-900 rounded-2xl shadow-md border border-zinc-800 hover:border-blue-500 group flex flex-col justify-between md:hover:scale-110 transition duration-500"
             >
               <div
                 className="absolute -top-4 -right-4 text-transparent text-4xl font-bold z-10 group-hover:opacity-0 opacity-100 transition-opacity duration-300"
@@ -221,21 +219,27 @@ export default function Home({ data }) {
               <div className="flex flex-col justify-between h-full">
                 <div className="">
                   <h3 className="text-lg text-zinc-200 font-medium group-hover:text-blue-300 transition-colors duration-500 capitalize">
-                    {result.book_title}
+                    {book.book_title}
                   </h3>
                   <p className="text-sm text-gray-500 line-clamp-1 group-hover:line-clamp-none">
-                    {result.author_name}
+                    {book.author_name}
                   </p>
                 </div>
                 <div className="my-4 flex items-center justify-between">
                   <p className="text-sm hidden md:block text-gray-500">
-                    Mentions: {result.mentions}
+                    Mentions: {book.mentions}
                   </p>
                   <p className="text-sm md:hidden text-gray-500">
-                    Mentioned {result.mentions} times
+                    Mentioned {book.mentions} times
                   </p>
                   <Link
-                    href={`https://www.amazon.com/dp/${result.amazon_id}`}
+                    href={
+                      book.amazon_id
+                        ? `https://www.amazon.com/dp/${book.amazon_id}`
+                        : `https://www.amazon.com/s?k=${encodeURIComponent(
+                            book.book_title
+                          )}`
+                    }
                     target="_blank"
                     className="bg-gray-600/50 px-2 py-0.5 rounded-md text-gray-400 hover:bg-blue-700 hover:text-gray-300 flex items-center "
                   >
@@ -247,19 +251,19 @@ export default function Home({ data }) {
                 <div className="flex text-sm text-gray-500 justify-between">
                   <div className="flex items-center">
                     <FaRegComment className="inline-block mr-1" />
-                    <span>{result.comments}</span>
+                    <span>{book.comments}</span>
                   </div>
                   <div className="flex items-center">
                     <FaHeart className="inline-block mr-1" />
-                    <span>{result.likes}</span>
+                    <span>{book.likes}</span>
                   </div>
                   <div className="flex items-center">
                     <IoIosStats className="inline-block mr-1" />
-                    <span>{formatImpressions(result.impressions)}</span>
+                    <span>{formatImpressions(book.impressions)}</span>
                   </div>
                   <div className="flex items-center">
                     <IoMdStar className="inline-block mr-1" />
-                    <span>{result.ratings || "N/A"}</span>
+                    <span>{book.ratings || "N/A"}</span>
                   </div>
                 </div>
               </div>
@@ -267,7 +271,7 @@ export default function Home({ data }) {
           ))}
         </div>
       )}
-    </div>
+    </>
   );
 }
 
@@ -276,7 +280,10 @@ export async function getStaticProps() {
 
   const data = db
     .prepare(
-      "SELECT id, book_title, author_name, mentions, comments, likes, impressions, amazon_id, olid, ratings, pages FROM aggregated_books"
+      `SELECT id, book_title, author_name, mentions, comments, likes, impressions, amazon_id, olid, ratings, pages,
+      (SELECT COUNT(*) FROM aggregated_books AS ab WHERE ab.author_name = aggregated_books.author_name) AS author_books,
+      (SELECT SUM(mentions) FROM aggregated_books AS ab WHERE ab.author_name = aggregated_books.author_name) AS author_mentions
+      FROM aggregated_books`
     )
     .all();
 
