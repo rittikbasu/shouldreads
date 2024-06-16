45b221ba860d5a3eccc49f0dbd664ee50197d5ec
rittikbasu <irittik@gmail.com>
2024-06-16 18:44:56 +0530

    feat: add vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import { Analytics } from "@vercel/analytics/react";
 import { useRouter } from "next/router";
 import { Work_Sans } from "next/font/google";
 import clsx from "clsx";
@@ -19,12 +20,7 @@ const work_sans = Work_Sans({
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   return (
-    <div
-      className={clsx(
-        work_sans.className,
-        "flex flex-col min-h-screen relative max-w-3xl mx-auto"
-      )}
-    >
+    <>
       <Head>
         <meta
           name="description"
@@ -47,34 +43,42 @@ export default function App({ Component, pageProps }) {
         />
         <meta name="twitter:image" content="/favicon.png" />
       </Head>
-      <header className="flex justify-center items-center w-full max-w-3xl mx-auto py-4 sm:py-8 relative">
-        {router.pathname !== "/" && (
-          <Link
-            href="/"
-            className="absolute left-8 sm:left-0 top-[63%] transform -translate-y-1/2"
-          >
-            <GradientIcon
-              IconComponent={FaArrowLeftLong}
-              svgClassName="w-10 h-10 sm:h-16 sm:w-16"
-              viewBox={"0 0 30 30"}
-            />
-          </Link>
+      <div
+        className={clsx(
+          work_sans.className,
+          "flex flex-col min-h-screen relative max-w-3xl mx-auto"
         )}
-        <h1 className="text-3xl sm:text-5xl bg-clip-text text-transparent bg-gradient-to-b from-zinc-100 via-zinc-100 to-zinc-500 font-mono font-medium">
-          shouldreads
-        </h1>
-        <div className="flex place-items-center absolute -top-[12rem] left-1/2 transform -translate-x-1/2 -z-10 before:h-[300px] before:w-[300px] before:translate-x-0 before:rounded-full before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[240px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:blur-2xl after:content-[''] before:bg-gradient-to-br before:from-transparent before:to-blue-700/10 after:from-sky-900 after:via-[#0141ff]/40 before:lg:h-[360px]"></div>
-      </header>
-      <main className="flex-grow flex flex-col items-center px-8 pb-8 pt-4 sm:pt-8">
-        <Component {...pageProps} />
-      </main>
-      <footer className="my-4 text-center text-sm sm:text-base text-zinc-500 font-mono">
-        <Link target="_blank" href="https://rittik.io" className="group">
-          <span>
-            by <span className="group-hover:text-blue-300">@_rittik</span>
-          </span>
-        </Link>
-      </footer>
-    </div>
+      >
+        <header className="flex justify-center items-center w-full max-w-3xl mx-auto py-4 sm:py-8 relative">
+          {router.pathname !== "/" && (
+            <Link
+              href="/"
+              className="absolute left-8 sm:left-0 top-[63%] transform -translate-y-1/2"
+            >
+              <GradientIcon
+                IconComponent={FaArrowLeftLong}
+                svgClassName="w-10 h-10 sm:h-16 sm:w-16"
+                viewBox={"0 0 30 30"}
+              />
+            </Link>
+          )}
+          <h1 className="text-3xl sm:text-5xl bg-clip-text text-transparent bg-gradient-to-b from-zinc-100 via-zinc-100 to-zinc-500 font-mono font-medium">
+            shouldreads
+          </h1>
+          <div className="flex place-items-center absolute -top-[12rem] left-1/2 transform -translate-x-1/2 -z-10 before:h-[300px] before:w-[300px] before:translate-x-0 before:rounded-full before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[240px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:blur-2xl after:content-[''] before:bg-gradient-to-br before:from-transparent before:to-blue-700/10 after:from-sky-900 after:via-[#0141ff]/40 before:lg:h-[360px]"></div>
+        </header>
+        <main className="flex-grow flex flex-col items-center px-8 pb-8 pt-4 sm:pt-8">
+          <Component {...pageProps} />
+        </main>
+        <footer className="my-4 text-center text-sm sm:text-base text-zinc-500 font-mono">
+          <Link target="_blank" href="https://rittik.io" className="group">
+            <span>
+              by <span className="group-hover:text-blue-300">@_rittik</span>
+            </span>
+          </Link>
+        </footer>
+      </div>
+      <Analytics />
+    </>
   );
 }
